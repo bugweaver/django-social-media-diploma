74b9f00d64fab6e7b560ec18c0c13f4d212a0ea6
anatoly <anatoly@gmail.com>
2024-08-30 15:39:38 +0300

    bug fixes

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     user = models.ForeignKey(User, related_name="posts", on_delete=models.DO_NOTHING)
-    body = models.CharField(max_length=200)
+    body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     video = models.FileField(upload_to='videos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
